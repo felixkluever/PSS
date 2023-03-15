d15ff592d391da4d9c1aa046b2eeea4a76c7d950
Felix Klüver <privat@felix-kluever.de>
2023-03-15 12:56:48 +0100

    lots of unitial UI

--- PSS/main.cpp
+++ PSS/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // translation
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
